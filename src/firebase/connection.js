a59ae098ebb568ff9669cbc58e53178c08870b59
joao-pedro-chapichi <chapichijp@gmail.com>
2025-04-29 15:48:49 -0300

    29-04 Nova atualizacao

---
+++ src/firebase/connection.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBpddCPyOPrvyTQmZP65-yx_anjKZAgopI",
+  authDomain: "appburger-2adfc.firebaseapp.com",
+  projectId: "appburger-2adfc",
+  storageBucket: "appburger-2adfc.firebasestorage.app",
+  messagingSenderId: "522288539675",
+  appId: "1:522288539675:web:0b538d6d2d9b7e4cb8b0be"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
