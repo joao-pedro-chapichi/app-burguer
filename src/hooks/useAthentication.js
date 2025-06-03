34c314d10490b4451f7ed6321bbcc9b67530cdf2
joao-pedro-chapichi <chapichijp@gmail.com>
2025-06-03 15:20:20 -0300

    Modificacao #1 - Aula 03-06

--- src/hooks/useAthentication.js
+++ src/hooks/useAthentication.js
@@ -4,65 +4,91 @@ import {
     signInWithEmailAndPassword,
     updateProfile,
     signOut
-  } from "firebase/auth";
-  import { useState, useEffect } from 'react';
-  import { db } from '../firebase/connection';
-   
-  export const useAuthentication = () => {
+} from "firebase/auth";
+import { useState, useEffect } from 'react';
+import { db } from '../firebase/connection';
+ 
+export const useAuthentication = () => {
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(null);
     const [cancelled, setCancelled] = useState(false);
-    const auth = getAuth();
-   
+    const auth = getAuth()
+ 
     function checkIfIsCancelled() {
-      if (cancelled) {
-        return;
-      }
-    }
-   
-    const createUser = async (data) => {
-      checkIfIsCancelled();
-      setLoading(true);
-      setError(null);
-   
-      try {
-        const { user } = await createUserWithEmailAndPassword(
-          auth,
-          data.displayEmail,
-          data.displayPassword
-        );
-   
-        await updateProfile(user, {
-          displayName: data.displayName
-        });
-   
-        return user;
-      } catch (error) {
-        let systemErrorMessage;
-   
-        if (error.message.includes("Password")) {
-          systemErrorMessage = "A senha precisa conter pelo menos 6 caracteres";
-        } else if (error.message.includes("email-already")) {
-          systemErrorMessage = "E-mail já cadastrado";
-        } else {
-          systemErrorMessage = "Ocorreu um erro - Tente Novamente";
+        if (cancelled) {
+            return;
         }
-   
-        setError(systemErrorMessage);
-      }
-      finally{
-        setLoading(false)
-      }
-    };
-   
+    }
+    //método para criar o usuário
+    const createUser = async (data) => {
+        checkIfIsCancelled();
+        setLoading(true);
+        setError(null);
+ 
+ 
+        try {
+            const { user } = await createUserWithEmailAndPassword(
+                auth, data.displayEmail, data.displayPassword
+            )
+ 
+            await updateProfile(user, {
+                displayName: data.displayName
+            })
+            return user
+        }
+        catch (error) {
+            let systemErrorMessage;
+            if (error.message.includes("Password")) {
+                systemErrorMessage = "A senha precisa conter pelo menos 6 caracteres";
+            }
+            else if (error.message.includes("email-already")) {
+                systemErrorMessage = "E-mail já cadastrado";
+            }
+            else {
+                systemErrorMessage = "Ocorreu um erro - Tente Novamente";
+            }
+            setError(systemErrorMessage);
+        } finally {
+            setLoading(false);
+        }
+    }
+ 
+    //método para logar ao projeto
+    const login = async (data) => {
+        checkIfIsCancelled()
+        setLoading(true)
+        setError(false)
+ 
+        try {
+            await signInWithEmailAndPassword(auth, data.email, data.password)
+ 
+        } catch (error) {
+            let systemErrorMessage;
+ 
+            if (error.message.includes("user-not-found")) {
+                systemErrorMessage = "Usuário não Cadastrado"
+            } else if (error.message.includes("wrong-password")) {
+                systemErrorMessage = "Senha Incorreta"
+            }
+            else {
+                systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde"
+            }
+            setError(systemErrorMessage)
+        } finally {
+            setLoading(false)
+        }
+    }
+ 
+ 
     useEffect(() => {
-      return () => setCancelled(true);
+        return () => setCancelled(true);
     }, []);
-   
+ 
     return {
-      createUser,
-      auth,
-      error,
-      loading
-    };
-  };
+        auth,
+        createUser,
+        error,
+        loading,
+        login
+    }
+}
